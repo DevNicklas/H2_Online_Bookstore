eb5a775ac7b6815b8ecb82e4823074c37d4141bd
janick004 <78735480+janick004@users.noreply.github.com>
2024-04-24 12:14:55 +0200

    CreateCustomers

--- queries/storedProcedures.sql
+++ queries/storedProcedures.sql
@@ -12,6 +12,48 @@ BEGIN
 	SELECT * FROM Books WHERE Title = bookTitle;
 END//
 
+--Customers START
+CREATE PROCEDURE IF NOT EXISTS CreateCustomers(
+    IN p_FirstName VARCHAR(30),
+    IN p_LastName VARCHAR(100),
+    IN p_Email VARCHAR(255),
+    IN p_PhoneNumber VARCHAR(15),
+    IN p_Country VARCHAR(100),
+    IN p_Region VARCHAR(50),
+    IN p_PostalCode VARCHAR(15),
+    IN p_StreetAddress VARCHAR(255),
+    IN p_Username VARCHAR(30),
+    IN p_PasswordHash VARCHAR(1023),
+    IN p_Salt VARCHAR(255)
+)
+BEGIN
+    DECLARE v_LoginID INT;
+
+    -- Check if PostalCode exists in Cities table
+    DECLARE v_Exists INT DEFAULT 0;
+    SELECT COUNT(*) INTO v_Exists FROM Citys WHERE PostalCode = p_PostalCode;
+    
+    IF v_Exists = 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'PostalCode does not exist in Cities table';
+    ELSE
+        -- Insert into LoginInfo table
+        INSERT INTO LoginInfo (Username, PasswordHash, Salt)
+        VALUES (p_Username, p_PasswordHash, p_Salt);
+
+        -- Get the generated LoginID
+        SET v_LoginID = LAST_INSERT_ID();
+
+        -- Insert into Customers table
+        INSERT INTO Customers (FirstName, LastName, Email, PhoneNumber, Country, Region, PostalCode, StreetAddress, LoginID)
+        VALUES (p_FirstName, p_LastName, p_Email, p_PhoneNumber, p_Country, p_Region, p_PostalCode, p_StreetAddress, v_LoginID);
+    END IF;
+END//
+
+
+
+
+--Customers END
+
 -- To show we meet the goals
 
 CREATE PROCEDURE TestProcedure()

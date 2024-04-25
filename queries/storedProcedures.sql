df85452b47a13bb1c5e37f5cef39ab35a4d70504
janick004 <78735480+janick004@users.noreply.github.com>
2024-04-25 10:22:27 +0200

    Update storedProcedures.sql

--- queries/storedProcedures.sql
+++ queries/storedProcedures.sql
@@ -23,38 +23,62 @@ BEGIN
 	SELECT * FROM Books WHERE Title = bookTitle;
 END//
 
+
+CREATE PROCEDURE GetCustomerByLoginID(
+    IN local_LoginID INT
+)
+BEGIN
+    DECLARE customerExists INT UNSIGNED DEFAULT 0;
+
+    -- Check if customer exists with the given LoginID
+    SELECT COUNT(*) INTO customerExists
+    FROM Customers
+    WHERE LoginID = local_LoginID;
+
+    -- If customer exists, return customer details
+    IF customerExists > 0 THEN
+        SELECT *
+        FROM Customers
+        WHERE LoginID = local_LoginID;
+    ELSE
+        -- If no customer found, return an empty result set
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No customer found with the provided LoginID';
+    END IF;
+END//
+
+
 CREATE PROCEDURE CreateCustomer(
-    IN p_FirstName VARCHAR(30),
-    IN p_LastName VARCHAR(100),
-    IN p_Email VARCHAR(255),
-    IN p_PhoneNumber VARCHAR(15),
-    IN p_Country VARCHAR(100),
-    IN p_Region VARCHAR(50),
-    IN p_PostalCode VARCHAR(15),
-    IN p_StreetAddress VARCHAR(255),
-    IN p_Username VARCHAR(30),
-    IN p_PasswordHash VARCHAR(1023),
-    IN p_Salt VARCHAR(255)
+    IN local_FirstName VARCHAR(30),
+    IN local_LastName VARCHAR(100),
+    IN local_Email VARCHAR(255),
+    IN local_PhoneNumber VARCHAR(15),
+    IN local_Country VARCHAR(100),
+    IN local_Region VARCHAR(50),
+    IN local_PostalCode VARCHAR(15),
+    IN local_StreetAddress VARCHAR(255),
+    IN local_Username VARCHAR(30),
+    IN local_PasswordHash VARCHAR(1023),
+    IN local_Salt VARCHAR(255)
 )
 BEGIN
     DECLARE v_LoginID INT;
 
     -- Check if PostalCode exists in Cities table
     DECLARE v_Exists INT DEFAULT 0;
-    SELECT COUNT(*) INTO v_Exists FROM Citys WHERE PostalCode = p_PostalCode;
+    SELECT COUNT(*) INTO v_Exists FROM Citys WHERE PostalCode = local_PostalCode;
     
     IF v_Exists = 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'PostalCode does not exist in Cities table';
     ELSE
         -- Insert into LoginInfo table
-        CALL CreateLogin(p_Username, p_PasswordHash, p_Salt);
+        CALL CreateLogin(local_Username, local_PasswordHash, local_Salt);
 
         -- Get the generated LoginID
         SET v_LoginID = LAST_INSERT_ID();
 
         -- Insert into Customers table
         INSERT INTO Customers (FirstName, LastName, Email, PhoneNumber, Country, Region, PostalCode, StreetAddress, LoginID)
-        VALUES (p_FirstName, p_LastName, p_Email, p_PhoneNumber, p_Country, p_Region, p_PostalCode, p_StreetAddress, v_LoginID);
+        VALUES (local_FirstName, local_LastName, local_Email, local_PhoneNumber, local_Country, local_Region, local_PostalCode, local_StreetAddress, v_LoginID);
     END IF;
 END//
 
@@ -86,29 +110,6 @@ BEGIN
     END IF;
 END//
 
-CREATE PROCEDURE GetCustomerByLoginID(
-    IN p_LoginID INT
-)
-BEGIN
-    DECLARE customerExists INT UNSIGNED DEFAULT 0;
-
-    -- Check if customer exists with the given LoginID
-    SELECT COUNT(*) INTO customerExists
-    FROM Customers
-    WHERE LoginID = p_LoginID;
-
-    -- If customer exists, return customer details
-    IF customerExists > 0 THEN
-        SELECT *
-        FROM Customers
-        WHERE LoginID = p_LoginID;
-    ELSE
-        -- If no customer found, return an empty result set
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No customer found with the provided LoginID';
-    END IF;
-END//
-
-	
 -- Create a login using Username, Password and Salt
 CREATE PROCEDURE CreateLogin(
 	IN inUsername VARCHAR(30),
@@ -266,7 +267,7 @@ DELIMITER ;
 
 -- Drop TestProcedure because we don't need it, but just want to show we meet the goals
 DROP PROCEDURE TestProcedure;
-
+CALL GetCustomerByLoginID(2);
 SELECT * FROM Customers;
 SELECT * FROM PurchaseLog;
 SELECT * FROM LoginInfo;
@@ -277,5 +278,5 @@ CALL AddPurchase(3, '978-87-28-00129-6');
 -- CALL CreateLogin('per', '421', 'ma');
 -- CALL GetLogin('nick');
 CALL DeleteLogin(2);
-CALL CreateCustomer('Peter', 'Gustavsen', 'testmail', '+45 22222222', 'Denmark', 'Sjælland', '4100', 'Ahorn Alle 3', 'peter', 'ok', 'fedtsalt');
+CALL CreateCustomer('1', 'Gustavsen', 'testmail', '+45 22222222', 'Denmark', null, '4100', 'Ahorn Alle 3', 'peter', 'ok', 'fedtsalt');
 CALL DeleteCustomer(3);
